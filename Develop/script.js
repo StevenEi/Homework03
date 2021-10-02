fdbf0ee80496471151dd3e197a3b1f37baf4863e
Steven Eiselt <seiselts@gmail.com>
2021-10-02 16:19:09 -0700

    First addition of JS, HTML, CSS and Readme files

--- Develop/script.js
+++ Develop/script.js
@@ -1,48 +1,59 @@
-// Assignment Code
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] // 10 characters
+var specCharacters = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '}', '~'] //28 characters
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] // 26 characters
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var possibleChars = [numbers, specCharacters, lowerCase, upperCase]
+var totalChars = []
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+function writePassword() {
+  var Initiate = confirm("Do you want to generate a new password?")
+  if (Initiate) {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }}
+  
+  function generatePassword() {
+    var genPassword = '';
+    var pwLength = parseInt(prompt("Please enter the desired password length. The length must be between 8 and 128 characters."))
+    if (pwLength > 128 || pwLength < 8) {
+      alert("Improper password length")
+      generatePassword()
+    }
+   
+    var pwNumbers = confirm("Do you want to include numeric characters in your password?")
+    if (pwNumbers === false) {
+      possibleChars.splice(0)
+    }
+    var pwLowercase = confirm("Do you want to include lowercase letters in your password?")
+    if (pwLowercase === false) {
+      possibleChars.splice(2)
+    }
+    var pwUppercase = confirm("Do you want to include uppercase letters in your password?")
+    if (pwUppercase === false) {
+      possibleChars.splice(3)
+    }
+    var pwSpecials = confirm("Do you want to include special characters in your password?")
+    if (pwSpecials === false) {
+      possibleChars.splice(1)
+    }
 
-
-
-
-var Initiate = confirm("Do you want to generate a new password?")
-  if(Initiate){
-      // Initiate the prompts pwLength -> charTypes ->
-  }
-  else {
-    return "Have a good day"
+    if (pwNumbers == false && pwLowercase == false && pwUppercase == false && pwSpecials == false) {
+      alert("You need to choose at least one character type in your password")
+    }
+    
+    for (var i = 0; i < pwLength; i++) {
+      numRandom = Math.floor(Math.random()*(possibleChars.length))
+      genPassword += possibleChars[numRandom]
+    };
+    return genPassword
   }
   
-var pwLength = prompt("Please enter the desired password length. The length must be between 8 and 128 characters.")
-  if(){
+  
 
-  }
-  else {
-    return "Invalid password length"
-  }
 
-var charTypes = prompt("Please choose which character types you want to include in your password")
-  if() {
 
-  }
-  else {
-  return "Invalid characters chosen"
-  }
-
-  // Need to have an array of possible numbers and characters and length based on input
-
-  var numbers = [0,1,2,3,4,5,6,7,8,9] // 10 characters
-  var specCharacters = ['!','"','#','$','%','&','(',')','*','+','-','.','/',':',';','<','=','>','?','@','[',']','^','_','`','{','}','~'] //28 characters
-  var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'] // 26 characters
-  var upperCase = str.toupperCase(letters)
-  var lowerCase = str.tolowerCase(letters)
 
---
+++ README.md
@@ -0,0 +1,23 @@
+Steven Eiselt
+03 Password Generator
+
+## Project Description
+For this project I had to create a website that automatically generated a password composed of random characters. The length and type of characters that are included in the password are to be determined by user choice via prompts after clicking a "generate password" button. 
+
+## What I Learned
+This project helped me learn a lot about for loops and when they should be applied in websites. I also learned a lot about how arrays and arrays within arrays interact and what methods we can use to manipulate them. However, I think that I made my system overly complex by making too many arrays rather than just having them be strings and concatanating them together. I also learned that it's often very useful to declare variables and leaving them empty at the beginning of the page and then filling them with values later on, it seems to make the script look and feel cleaner.
+
+## Screenshot of Deployed Website
+
+
+## URL to Deployed Website
+
+
+## Difficulties that I had
+=Undefined Outputs=
+    I had a lot of trouble having some values be "undefined" on the website after going through my calculator. I will need to figure out why that is happening in the near future.
+    
+
+
+
+
