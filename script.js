78780bf82713064e618edec6cd22d99bbfbadf84
Steven Eiselt <seiselts@gmail.com>
2021-10-03 13:04:30 -0700

    directory cleanup

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] // 10 characters
-var specCharacters = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '}', '~'] //28 characters
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] // 26 characters
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-var possibleChars = [numbers, specCharacters, lowerCase, upperCase]
-var totalChars = []
+var numbers = "0123456789";
+var specCharacters = "!#$%&()*+-./:;<=>?@[]^_`{,}~"
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var upperCase = lowerCase.toUpperCase();
 
 var generateBtn = document.querySelector("#generate");
 
@@ -17,37 +15,38 @@ function writePassword() {
   }}
   
   function generatePassword() {
-    var genPassword = '';
+    var possibleChars = " "
+    var genPassword = " ";
     var pwLength = parseInt(prompt("Please enter the desired password length. The length must be between 8 and 128 characters."))
     if (pwLength > 128 || pwLength < 8) {
       alert("Improper password length")
-      generatePassword()
+      return generatePassword()
     }
    
     var pwNumbers = confirm("Do you want to include numeric characters in your password?")
-    if (pwNumbers === false) {
-      possibleChars.splice(0)
-    }
+    if (pwNumbers) {
+      possibleChars += numbers;
+    };
     var pwLowercase = confirm("Do you want to include lowercase letters in your password?")
-    if (pwLowercase === false) {
-      possibleChars.splice(2)
-    }
+    if (pwLowercase) {
+      possibleChars += lowerCase;
+    };
     var pwUppercase = confirm("Do you want to include uppercase letters in your password?")
-    if (pwUppercase === false) {
-      possibleChars.splice(3)
-    }
+    if (pwUppercase) {
+      possibleChars += upperCase;
+    };
     var pwSpecials = confirm("Do you want to include special characters in your password?")
-    if (pwSpecials === false) {
-      possibleChars.splice(1)
-    }
+    if (pwSpecials) {
+      possibleChars += specCharacters;
+    };
 
     if (pwNumbers == false && pwLowercase == false && pwUppercase == false && pwSpecials == false) {
       alert("You need to choose at least one character type in your password")
-    }
+      return generatePassword()
+    };
     
     for (var i = 0; i < pwLength; i++) {
-      numRandom = Math.floor(Math.random()*(possibleChars.length))
-      genPassword += possibleChars[numRandom]
+      genPassword += possibleChars.charAt(Math.floor(Math.random()*possibleChars.length))
     };
     return genPassword
   }
